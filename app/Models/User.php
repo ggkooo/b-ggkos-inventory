9faf1986caa6f5b1ab09613d06e11875799d0849
Giordano Bruno Biasi Berwig <giordano.berwig@sou.unijui.edu.br>
2026-04-27 11:10:55 -0300

    feat(users): add sanctum tokens and uuid support fields

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,16 +6,26 @@ namespace App\Models;
 use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Attributes\Hidden;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
-#[Fillable(['name', 'username', 'email', 'phone', 'cpf', 'password', 'admin'])]
+#[Fillable(['uuid', 'name', 'username', 'email', 'phone', 'cpf', 'password', 'admin'])]
 #[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable
 {
     /** @use HasFactory<UserFactory> */
-    use HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, HasUuids, Notifiable;
+
+    /**
+     * @return array<int, string>
+     */
+    public function uniqueIds(): array
+    {
+        return ['uuid'];
+    }
 
     /**
      * Get the attributes that should be cast.
